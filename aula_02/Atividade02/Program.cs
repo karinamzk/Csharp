8938467214cc2cd12afe0229612a5305a0e0f574
Karina Akina <karinamzk2@gmail.com>
2023-08-23 15:23:59 -0300

    Atualição aula 02

--- aula_02/Atividade02/Program.cs
+++ aula_02/Atividade02/Program.cs
@@ -5,26 +5,26 @@
         static void Main(string[] args)
         {
             float nota1, nota2, nota3, nota4, media;
-            //string nomealuno;
+            string nomealuno;
 
-            //Console.WriteLine(nomealuno);
-            //nomealuno = cConsole.ReadLine();//
+            Console.WriteLine("Nome do aluno: ");
+            nomealuno = Console.ReadLine();
 
             Console.WriteLine("Escreva a nota 1: ");
             nota1 = Convert.ToSingle(Console.ReadLine());
 
-            Console.WriteLine("Escreva a nota 1: ");
+            Console.WriteLine("Escreva a nota 2: ");
             nota2 = Convert.ToSingle(Console.ReadLine());
 
-            Console.WriteLine("Escreva a nota 1: ");
+            Console.WriteLine("Escreva a nota 3: ");
             nota3 = Convert.ToSingle(Console.ReadLine());
 
-            Console.WriteLine("Escreva a nota 1: ");
+            Console.WriteLine("Escreva a nota 4: ");
             nota4 = Convert.ToSingle(Console.ReadLine());
 
             media = (nota1 + nota2 + nota3 + nota4)/4;
 
-            Console.WriteLine("Sua media é: " + media);
+            Console.WriteLine("Média do aluno "+ nomealuno + "é:" + media);
          
         }
     }
